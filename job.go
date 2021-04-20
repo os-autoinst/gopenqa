07eb00374e2dc3acd7d02da47f0bab0945033905
felix.niederwanger@suse.com <phoenix>
2021-04-20 09:33:40 +0200

    Improve job tests
    Add individual jobs to the test suite

--- job.go
+++ job.go
@@ -1,5 +1,7 @@
 package gopenqa
 
+import "fmt"
+
 /* Job instance */
 type Job struct {
 	AssignedWorkerID int      `json:"assigned_worker_id"`
@@ -37,4 +39,32 @@ type Settings struct {
 	Arch    string `json:"ARCH"`
 	Backend string `json:"BACKEND"`
 	Machine string `json:"MACHINE"`
+}
+
+/* Format job as a string */
+func (j *Job) String() string {
+	return fmt.Sprintf("%d %s (%s)", j.ID, j.Name, j.Test)
+}
+func (j *Job) JobState() string {
+	if j.State == "done" {
+		return j.Result
+	}
+	return j.State
+}
+
+/* Compares two jobs according to their unique parameters (ID, GroupID, Test) */
+func (j1 *Job) Equals(j2 Job) bool {
+	// Compare only relevant parameters
+	if j1.ID != j2.ID {
+		return false
+	}
+	if j1.GroupID != j2.GroupID {
+		return false
+	}
+	if j1.Test != j2.Test {
+		return false
+	}
+	// Consider the other parameters as volatile and thus not important
+
+	return true
 }
